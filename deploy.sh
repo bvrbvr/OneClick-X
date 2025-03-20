af3f6924678a131ea5f7c486a7558150810b4a6e
Alexey Buslaev <gamer23@yandex.ru>
2025-03-20 10:56:16 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,32 +15,32 @@ echo
 
 if [ "$1" ]; then
     if [[ $1 =~ $EMAIL_REGEX ]]; then
-        docker kill xray-shadowsocks-vless 2>/dev/null
-        docker rm xray-shadowsocks-vless 2>/dev/null
-        docker image rm xray-shadowsocks-vless 2>/dev/null
+        docker kill OneClick-X 2>/dev/null
+        docker rm OneClick-X 2>/dev/null
+        docker image rm OneClick-X 2>/dev/null
         docker volume rm xray_vol 2>/dev/null
         CLIENT_EMAIL=$1
     else
         if [ "$1" == "remove" ]; then
-            docker kill xray-shadowsocks-vless 2>/dev/null
-            docker rm xray-shadowsocks-vless 2>/dev/null
-            docker image rm xray-shadowsocks-vless 2>/dev/null
+            docker kill OneClick-X 2>/dev/null
+            docker rm OneClick-X 2>/dev/null
+            docker image rm OneClick-X 2>/dev/null
             docker volume rm xray_vol 2>/dev/null
-            echo "Xray-shadowsocks-vless removed."
+            echo "OneClick-X removed."
             exit 0
         else
             if [ "$1" == "reload" ]; then
-                docker kill xray-shadowsocks-vless 2>/dev/null
-                docker rm xray-shadowsocks-vless 2>/dev/null
-                docker image rm xray-shadowsocks-vless 2>/dev/null
+                docker kill OneClick-X 2>/dev/null
+                docker rm OneClick-X 2>/dev/null
+                docker image rm OneClick-X 2>/dev/null
                 docker volume rm xray_vol 2>/dev/null
             else
                 if [ "$1" == "restart" ]; then
-                    docker kill xray-shadowsocks-vless 2>/dev/null
-                    docker rm xray-shadowsocks-vless 2>/dev/null
+                    docker kill OneClick-X 2>/dev/null
+                    docker rm OneClick-X 2>/dev/null
                 else
                     if [ "$1" == "uuid" ]; then
-                        docker exec -it xray-shadowsocks-vless /opt/xray/xray uuid 2>/dev/null || echo "Please deploy xray server first."
+                        docker exec -it OneClick-X /opt/xray/xray uuid 2>/dev/null || echo "Please deploy Xray server first."
                         exit 0
                     else
                         echo "Wrong email or param was provided, performing regular deploy command instead."
@@ -53,8 +53,8 @@ if [ "$1" ]; then
     fi
 fi
 
-docker image ls | grep "xray-shadowsocks-vless" || docker build \
-    -t xray-shadowsocks-vless . \
+docker image ls | grep "OneClick-X" || docker build \
+    -t OneClick-X . \
     --build-arg RELOAD_BUST=$(date +%s) \
     --build-arg CLIENT_EMAIL=$CLIENT_EMAIL
 docker run \
@@ -62,20 +62,20 @@ docker run \
     -p 0.0.0.0:443:443/tcp \
     -p 0.0.0.0:23:23/tcp \
     -p 0.0.0.0:23:23/udp \
-    -d --name xray-shadowsocks-vless \
+    -d --name OneClick-X \
     --restart always \
-    xray-shadowsocks-vless 2>/dev/null || echo "Xray container already exists"
+    OneClick-X 2>/dev/null || echo "Xray container already exists"
 
 echo
 echo "########################### Xray config ###########################"
 echo
-docker exec -it xray-shadowsocks-vless cat /opt/xray/xray-creds.txt
+docker exec -it OneClick-X cat /opt/xray/xray-creds.txt
 echo
 echo "###################################################################"
 echo
 echo "####################### Xray connection url #######################"
 echo
-docker exec -it xray-shadowsocks-vless cat /opt/xray/vless-connection-string.txt | sed -r "s/127.0.0.1/"$(hostname -I | cut -d " " -f 1)"/"
+docker exec -it OneClick-X cat /opt/xray/vless-connection-string.txt | sed -r "s/127.0.0.1/"$(hostname -I | cut -d " " -f 1)"/"
 echo
 echo "###################################################################"
 echo
