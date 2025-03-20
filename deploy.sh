45b4732e35caf71870e5e54044e1a846ef270360
Alexey Buslaev <gamer23@yandex.ru>
2025-03-20 15:27:09 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@
 CLIENT_EMAIL="shadowuser@shadowserver.com"
 EMAIL_REGEX="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
 
+IMAGE_NAME="oneclick-x"
+CONTAINER_NAME="oneclick-x"
+VOLUME_NAME="xray_vol"
+
 if [ -x "$(command -v docker)" ]; then
     docker -v
 else
@@ -15,32 +19,32 @@ echo
 
 if [ "$1" ]; then
     if [[ $1 =~ $EMAIL_REGEX ]]; then
-        docker kill OneClick-X 2>/dev/null
-        docker rm OneClick-X 2>/dev/null
-        docker image rm OneClick-X 2>/dev/null
-        docker volume rm xray_vol 2>/dev/null
+        docker kill $CONTAINER_NAME 2>/dev/null
+        docker rm $CONTAINER_NAME 2>/dev/null
+        docker image rm $IMAGE_NAME 2>/dev/null
+        docker volume rm $VOLUME_NAME 2>/dev/null
         CLIENT_EMAIL=$1
     else
         if [ "$1" == "remove" ]; then
-            docker kill OneClick-X 2>/dev/null
-            docker rm OneClick-X 2>/dev/null
-            docker image rm OneClick-X 2>/dev/null
-            docker volume rm xray_vol 2>/dev/null
-            echo "OneClick-X removed."
+            docker kill $CONTAINER_NAME 2>/dev/null
+            docker rm $CONTAINER_NAME 2>/dev/null
+            docker image rm $IMAGE_NAME 2>/dev/null
+            docker volume rm $VOLUME_NAME 2>/dev/null
+            echo "$CONTAINER_NAME removed."
             exit 0
         else
             if [ "$1" == "reload" ]; then
-                docker kill OneClick-X 2>/dev/null
-                docker rm OneClick-X 2>/dev/null
-                docker image rm OneClick-X 2>/dev/null
-                docker volume rm xray_vol 2>/dev/null
+                docker kill $CONTAINER_NAME 2>/dev/null
+                docker rm $CONTAINER_NAME 2>/dev/null
+                docker image rm $IMAGE_NAME 2>/dev/null
+                docker volume rm $VOLUME_NAME 2>/dev/null
             else
                 if [ "$1" == "restart" ]; then
-                    docker kill OneClick-X 2>/dev/null
-                    docker rm OneClick-X 2>/dev/null
+                    docker kill $CONTAINER_NAME 2>/dev/null
+                    docker rm $CONTAINER_NAME 2>/dev/null
                 else
                     if [ "$1" == "uuid" ]; then
-                        docker exec -it OneClick-X /opt/xray/xray uuid 2>/dev/null || echo "Please deploy Xray server first."
+                        docker exec -it $CONTAINER_NAME /opt/xray/xray uuid 2>/dev/null || echo "Please deploy Xray server first."
                         exit 0
                     else
                         echo "Wrong email or param was provided, performing regular deploy command instead."
@@ -53,29 +57,29 @@ if [ "$1" ]; then
     fi
 fi
 
-docker image ls | grep "OneClick-X" || docker build \
-    -t OneClick-X . \
+docker image ls | grep "$IMAGE_NAME" || docker build \
+    -t $IMAGE_NAME . \
     --build-arg RELOAD_BUST=$(date +%s) \
     --build-arg CLIENT_EMAIL=$CLIENT_EMAIL
 docker run \
-    -v xray_vol:/opt/xray \
+    -v $VOLUME_NAME:/opt/xray \
     -p 0.0.0.0:443:443/tcp \
     -p 0.0.0.0:23:23/tcp \
     -p 0.0.0.0:23:23/udp \
-    -d --name OneClick-X \
+    -d --name $CONTAINER_NAME \
     --restart always \
-    OneClick-X 2>/dev/null || echo "Xray container already exists"
+    $IMAGE_NAME 2>/dev/null || echo "Xray container already exists"
 
 echo
 echo "########################### Xray config ###########################"
 echo
-docker exec -it OneClick-X cat /opt/xray/xray-creds.txt
+docker exec -it $CONTAINER_NAME cat /opt/xray/xray-creds.txt
 echo
 echo "###################################################################"
 echo
 echo "####################### Xray connection url #######################"
 echo
-docker exec -it OneClick-X cat /opt/xray/vless-connection-string.txt | sed -r "s/127.0.0.1/"$(hostname -I | cut -d " " -f 1)"/"
+docker exec -it $CONTAINER_NAME cat /opt/xray/vless-connection-string.txt | sed -r "s/127.0.0.1/"$(hostname -I | cut -d " " -f 1)"/"
 echo
 echo "###################################################################"
 echo
